f1243a8bd59cc573fc20e15d7295096e3c946e85
DavidKorochik <david1korochik@gmail.com>
2021-09-20 20:21:47 +0300

    Added to controllers folder and routes folder the needed files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { sequelize } = require('./models/index');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, async () => {
+  console.log(`App is listening in port ${process.env.PORT}`);
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the database');
+  } catch (err) {
+    console.error('Unable to connect to the database');
+  }
+});

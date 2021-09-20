ea1dd0da881365ef812b03dbda597056a4bb7f8f
DavidKorochik <david1korochik@gmail.com>
2021-09-20 20:48:32 +0300

    Finished usersController file

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,43 @@
 const { User } = require('../models/index');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    if (!name && !email && !password) {
+      return res
+        .status(400)
+        .json({ error: 'One or more fields are not fulfilled!' });
+    }
+
+    let user = await User.findAll({ where: { email } });
+
+    if (user) {
+      return res.status(401).json({ error: 'User already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = await User.create({ name, email, password: hashedPassword });
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.SECRET_KEY,
+      { expiresIn: 3600000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
